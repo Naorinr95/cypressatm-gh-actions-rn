da859b0a6ee1487eaec47f3e19da1bd4b4b42d0d
Naorinr95 <naorin.rifat95@gmail.com>
2025-05-13 15:37:53 +0600

    Add Cypress GitHub Actions workflow

--- cypress/e2e/atmtest.cy.js
+++ cypress/e2e/atmtest.cy.js
@@ -10,7 +10,7 @@ describe('CSSLocators', () => {
         cy.visit("http://www.automationpractice.pl/index.php")
         cy.get("#search_query_top").type("T-Shirts")
         cy.get("button[name='submit_search']").click()
-        cy.get(".lighter").contains("Dresses")
+        cy.get(".lighter").contains("T-Shirts")
       })
 
   })
