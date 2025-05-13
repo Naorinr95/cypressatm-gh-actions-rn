df70f805868063cadb3fc4338ff52abd3f3027c2
Naorinr95 <naorin.rifat95@gmail.com>
2025-05-13 15:57:01 +0600

    Add Cypress GH  Actions workflow

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter' ,
+  reporter: 'cypress-mochawesome-reporter',
   video: true,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      
       require('cypress-mochawesome-reporter/plugin')(on);
     },
     experimentalStudio: true,
--- cypress/e2e/atmtest.cy.js
+++ cypress/e2e/atmtest.cy.js
@@ -1,16 +1,19 @@
 describe('CSSLocators', () => {
-    it('csslocators', () => {
-      cy.visit("http://www.automationpractice.pl/index.php")
-      cy.get("#search_query_top").type("Dresses")
-      cy.get("button[name='submit_search']").click()
-      cy.get(".lighter").contains("Dresses")
-    })
+  it('searches for Dresses and verifies result', () => {
+    cy.visit("http://www.automationpractice.pl/index.php");
+    cy.get("#search_query_top").type("Dresses");
+    cy.get("button[name='submit_search']").click();
 
-    it('csslocators', () => {
-        cy.visit("http://www.automationpractice.pl/index.php")
-        cy.get("#search_query_top").type("T-Shirts")
-        cy.get("button[name='submit_search']").click()
-        cy.get(".lighter").contains("T-Shirts")
-      })
+    // Wait for result element and check text
+    cy.get(".lighter", { timeout: 10000 }).should('contain.text', "Dresses");
+  });
 
-  })
+  it('searches for T-Shirts and verifies result', () => {
+    cy.visit("http://www.automationpractice.pl/index.php");
+    cy.get("#search_query_top").type("T-Shirts");
+    cy.get("button[name='submit_search']").click();
+
+    // Wait for result element and check text
+    cy.get(".lighter", { timeout: 10000 }).should('contain.text', "T-Shirts");
+  });
+});
