3a332c67704838af7c7e016d3fbb1c79e8080817
Naorinr95 <naorin.rifat95@gmail.com>
2025-05-14 20:40:33 +0600

    Pass Case

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,12 @@
-const { defineConfig } = require('cypress');
-const mochawesome = require('cypress-mochawesome-reporter/plugin');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'mochawesome-report/.jsons', // ✅ Correct output folder
-    overwrite: false,
-    html: false,
-    json: true
-  },
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
-      mochawesome(on);
-      return config;
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
+    experimentalStudio: true,
   },
 });
--- cypress/e2e/atmtest.cy.js
+++ cypress/e2e/atmtest.cy.js
@@ -1,15 +1,15 @@
 describe('CSSLocators', () => {
-  it('passes', () => {
-    cy.visit('http://automationpractice.pl/index.php');
-    cy.get('#search_query_top').type('Dress');
+  it('searches for Dresses and verifies result', () => {
+    cy.visit("http://www.automationpractice.pl/index.php");
+    cy.get("#search_query_top").type("Dresses");
     cy.get("button[name='submit_search']").click();
-    cy.get('.lighter').should('contain.text', 'Dress');
+    cy.get(".lighter", { timeout: 10000 }).should('contain.text', "Dresses");
   });
 
-  it('fails', () => {
-    cy.visit('http://automationpractice.pl/index.php');
-    cy.get('#search_query_top').type('Shoes');
+  it('searches for T-Shirts and verifies result', () => {
+    cy.visit("http://www.automationpractice.pl/index.php");
+    cy.get("#search_query_top").type("T-Shirts");
     cy.get("button[name='submit_search']").click();
-    cy.get('.lighter').should('contain.text', 'NonExistentText');
+    cy.get(".lighter", { timeout: 10000 }).should('contain.text', "T-Shirts");
   });
 });
